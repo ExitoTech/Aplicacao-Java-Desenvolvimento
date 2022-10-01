083d9e5df93018a9f1239c3e5af1ba5b9fb0c152
VinyProgramador <vinicius.nunes@sptech.school>
2022-10-01 19:17:21 -0300

    Começamdo funçaõ para salvar de 3 em 3 seg.

--- exitoTech-jar/src/main/java/com/mycompany/exitotech/jar/conexaojdbc/ConexaoDAO.java
+++ exitoTech-jar/src/main/java/com/mycompany/exitotech/jar/conexaojdbc/ConexaoDAO.java
@@ -1,5 +1,6 @@
 package com.mycompany.exitotech.jar.conexaojdbc;
 
+import com.github.britooo.looca.api.core.Looca;
 import com.sun.jdi.connect.spi.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,34 +9,33 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.sql.RowSet;
 
-
 public class ConexaoDAO {
 
     private String url = "jdbc:mysql://localhost:3306/bdExitoTech";
     private String user = "root";
-    private String password = "V1k1ng1!";
+    private String password = "vini@123";
     private java.sql.Connection con = null;
-        
-    public void conexaoMysql(){
+
+    public void conexaoMysql() {
         try {
 //            JOptionPane.showConfirmDialog(null, "Inicializando conexão");
             System.out.println("Inicializando conexão");
-            con = DriverManager.getConnection(url,user,password);
+            con = DriverManager.getConnection(url, user, password);
 //            JOptionPane.showMessageDialog(null, "Conexão realizada!");
-             System.out.println("Conexão realizada!");
+            System.out.println("Conexão realizada!");
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Erro está localizado na classe de 'conexãoDAO'" + ex.getMessage());
         }
 
     }
-    
-        public void entrar(String email,String senha){
-            
-            String query = "SELECT * FROM empresa WHERE emailempresa = '" + email + "' AND senhaempresa = '" + senha + "' ;";
-            
+
+    public void entrar(String email, String senha) {
+
+        String query = "SELECT * FROM empresa WHERE emailempresa = '" + email + "' AND senhaempresa = '" + senha + "' ;";
+
         try {
             ResultSet resultSet = con.createStatement().executeQuery(query);
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 System.out.println("Entrando com usuario " + resultSet.getString("Nome"));
             }
         } catch (SQLException e) {
@@ -43,21 +43,29 @@ public class ConexaoDAO {
         }
     }
 
-        public  void Insert(String nome,String Cnpj,String email,String senha,String porte,String Logradouro,String uf,String CEP){
-            
-            String query = String.format(" INSERT INTO empresa (nome,cnpj,emailempresa,senhaempresa,porteempresa,logradouro,uf,cep)"
-                    + "Values ('%s','%s','%s','%s','%s','%s','%s','%s');", nome,Cnpj,email,senha,porte,Logradouro,uf,CEP);
-            
+    public void Insert(String nome, String Cnpj, String email, String senha, String porte, String Logradouro, String uf, String CEP) {
+        String query = String.format(" INSERT INTO empresa (nome,cnpj,emailempresa,senhaempresa,porteempresa,logradouro,uf,cep)"
+                + "Values ('%s','%s','%s','%s','%s','%s','%s','%s');", nome, Cnpj, email, senha, porte, Logradouro, uf, CEP);
         try {
-            
             Statement st = con.createStatement();
-            int m  = st.executeUpdate(query);
-            
-            if(m == 1){
-                System.out.println("Inserindo na tabela empresa:" +  query);
+            int m = st.executeUpdate(query);
+            if (m == 1) {
+                System.out.println("Inserindo na tabela empresa:" + query);
             }
         } catch (SQLException e) {
             System.out.println("Erro na classe 'conexãoDAO', metodo 'Insert'. " + e.getMessage());
         }
+    }
+    
+  public void ExtrairDados(){
+      Integer n = 0;
+        while (true) {            
+            n++;
+            System.out.println("Contagem do numero: "+n);
+            try {
+                Thread.sleep(3000);
+            } catch (Exception error) {
+            }
         }
+  }
 }
