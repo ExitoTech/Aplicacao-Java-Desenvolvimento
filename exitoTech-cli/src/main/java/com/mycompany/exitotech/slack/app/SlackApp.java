ed12f41c48c62970d34aca9e84bbba311fb6c7c0
Pedro <pedro.pires@sptech.school>
2022-11-17 20:33:08 -0300

    Slack pegando média das capturas no cli

--- exitoTech-cli/src/main/java/com/mycompany/exitotech/slack/app/SlackApp.java
+++ exitoTech-cli/src/main/java/com/mycompany/exitotech/slack/app/SlackApp.java
@@ -5,8 +5,13 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import org.jfree.data.json.impl.JSONObject;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
@@ -17,40 +22,72 @@ public class SlackApp {
     private static HttpClient client = HttpClient.newHttpClient();
     private static String url;
     
-    
-    
     public static void validacao(Integer id_maquina, Double usoProcessador, Long usoRam) throws IOException, InterruptedException{
         ConexaoDAO connection = new ConexaoDAO();
         connection.conexaoMysql();
         JdbcTemplate con = connection.getConnection();
-        String query = String.format("SELECT emp.webHook AS webHook\n" +
+        String query1 = String.format("SELECT emp.webHook AS webHook\n" +
                         "FROM maquina AS maq \n" +
                         "JOIN setor AS ser\n" +
                         "ON ser.idSetor = maq.fk_setor\n" +
                         "RIGHT JOIN empresa AS emp\n" +
                         "ON ser.fk_Empresa = emp.idEmpresa WHERE idMaquina = %d;", id_maquina);
-        List webHook = con.queryForList(query);
+        
+        String queryRam = String.format("SELECT avg(usoRam) FROM capturas WHERE "
+                + "idCaptura in (SELECT TOP 5 idCaptura FROM capturas ORDER BY idCaptura DESC);", id_maquina);
+        
+        String queryCpu = String.format("SELECT avg(usoCpu) FROM capturas WHERE "
+                + "idCaptura in (SELECT TOP 5 idCaptura FROM capturas ORDER BY idCaptura DESC)", id_maquina);
+        
+        List webHook = con.queryForList(query1);
+        List capturaCpu = con.queryForList(queryCpu);
+        List capturaRam = con.queryForList(queryRam);
+        
         String wh = webHook.toString().replace("[{webHook=", "").replace("}]", "");
         
-        if(wh == null || wh.equals("")){
-            System.out.println("\nNenhum Slack cadastrado/encontrado.\n");
-        }else{
-            url = wh;
-            JSONObject json = new JSONObject();
-            String usoCpu = String.format("%.0f", usoProcessador);
-
-            if(usoProcessador > 75){
-                json.put("text", "A máquina: " + id_maquina + " está com o uso da CPU acima do normal, seria interessante verificar a máquina."
-                        + "\nUso da CPU: " + usoCpu + "%");
-                enviarMensagem(json);
-            }
-
-            if(usoRam > 75){
-                json.put("text", "A máquina: " + id_maquina + " está com o uso da memória RAM acima do normal, é interessante verificar a máquina."
-                        + "\nUso da memória RAM: " + usoRam + "%");
-                enviarMensagem(json);
-            }
-        }
+        System.out.println(capturaCpu);
+        //System.out.println(capturaCpu);
+//        Timer timer =  new Timer();
+//        TimerTask slack = new TimerTask() {
+//            @Override
+//            public void run() {
+//                List capturaRam = con.queryForList(queryRam);
+//                List capturaCpu = con.queryForList(queryCpu);
+//                
+//                if(wh == null || wh.equals("")){
+//                    System.out.println("\nNenhum Slack cadastrado/encontrado.\n");
+//                }else{
+//                    url = wh;
+//                    JSONObject json = new JSONObject();
+//                    String usoCpu = String.format("%.0f", usoProcessador);
+//
+//                    if(usoProcessador > 1){
+//                        json.put("text", "A máquina: " + id_maquina + " está com o uso da CPU acima do normal, seria interessante verificar a máquina."
+//                                + "\nUso da CPU: " + usoCpu + "%");
+//                        try {
+//                            enviarMensagem(json);
+//                        } catch (IOException ex) {
+//                            Logger.getLogger(SlackApp.class.getName()).log(Level.SEVERE, null, ex);
+//                        } catch (InterruptedException ex) {
+//                            Logger.getLogger(SlackApp.class.getName()).log(Level.SEVERE, null, ex);
+//                        }
+//                    }
+//
+//                    if(usoRam > 1){
+//                        try {
+//                            json.put("text", "A máquina: " + id_maquina + " está com o uso da memória RAM acima do normal, é interessante verificar a máquina."
+//                                    + "\nUso da memória RAM: " + usoRam + "%");
+//                            enviarMensagem(json);
+//                        } catch (IOException ex) {
+//                            Logger.getLogger(SlackApp.class.getName()).log(Level.SEVERE, null, ex);
+//                        } catch (InterruptedException ex) {
+//                            Logger.getLogger(SlackApp.class.getName()).log(Level.SEVERE, null, ex);
+//                        }
+//                    }
+//                }
+//            }
+//        };
+//        timer.scheduleAtFixedRate(slack, 0, 30000);
     }
 
     public static void enviarMensagem(JSONObject content) throws IOException, InterruptedException{
--- exitoTech-jar/src/main/java/com/mycompany/exitotech/slack/app/SlackApp.java
+++ exitoTech-jar/src/main/java/com/mycompany/exitotech/slack/app/SlackApp.java
@@ -7,6 +7,10 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import org.jfree.data.json.impl.JSONObject;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
@@ -17,40 +21,53 @@ public class SlackApp {
     private static HttpClient client = HttpClient.newHttpClient();
     private static String url;
     
-    
-    
     public static void validacao(Integer id_maquina, Double usoProcessador, Long usoRam) throws IOException, InterruptedException{
         ConexaoDAO connection = new ConexaoDAO();
         connection.conexaoMysqlLocal();
         JdbcTemplate con = connection.getConnection();
-        String query = String.format("SELECT emp.webHook AS webHook\n" +
+        String query1 = String.format("SELECT emp.webHook AS webHook\n" +
                         "FROM maquina AS maq \n" +
                         "JOIN setor AS ser\n" +
                         "ON ser.idSetor = maq.fk_setor\n" +
                         "RIGHT JOIN empresa AS emp\n" +
                         "ON ser.fk_Empresa = emp.idEmpresa WHERE idMaquina = %d;", id_maquina);
-        List webHook = con.queryForList(query);
+        
+        String query2 = String.format("select top 5 usoCPu, usoRam, idCaptura from "
+                + "capturas where fk_maquina = %d order by idCaptura desc;", id_maquina);
+        List webHook = con.queryForList(query1);
         String wh = webHook.toString().replace("[{webHook=", "").replace("}]", "");
         
-        if(wh == null || wh.equals("")){
-            System.out.println("\n Nenhum Slack cadastrado/encontrado.\n");
-        }else{
-            url = wh;
-            JSONObject json = new JSONObject();
-            String usoCpu = String.format("%.0f", usoProcessador);
+                if(wh == null || wh.equals("")){
+                    System.out.println("\n Nenhum Slack cadastrado/encontrado.\n");
+                }else{
+                    url = wh;
+                    JSONObject json = new JSONObject();
+                    String usoCpu = String.format("%.0f", usoProcessador);
 
-            if(usoProcessador > 1){
-                json.put("text", "A máquina: " + id_maquina + " está com o uso da CPU acima do normal, seria interessante verificar a máquina."
-                        + "\nUso da CPU: " + usoCpu + "%");
-                enviarMensagem(json);
-            }
+                    if(usoProcessador > 1){
+                        json.put("text", "A máquina: " + id_maquina + " está com o uso da CPU acima do normal, seria interessante verificar a máquina."
+                                + "\nUso da CPU: " + usoCpu + "%");
+                        try {
+                            enviarMensagem(json);
+                        } catch (IOException ex) {
+                            Logger.getLogger(SlackApp.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(SlackApp.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
 
-            if(usoRam > 1){
-                json.put("text", "A máquina: " + id_maquina + " está com o uso da memória RAM acima do normal, é interessante verificar a máquina."
-                        + "\nUso da memória RAM: " + usoRam + "%");
-                enviarMensagem(json);
-            }
-        }
+                    if(usoRam > 1){
+                        try {
+                            json.put("text", "A máquina: " + id_maquina + " está com o uso da memória RAM acima do normal, é interessante verificar a máquina."
+                                    + "\nUso da memória RAM: " + usoRam + "%");
+                            enviarMensagem(json);
+                        } catch (IOException ex) {
+                            Logger.getLogger(SlackApp.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(SlackApp.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
     }
 
     public static void enviarMensagem(JSONObject content) throws IOException, InterruptedException{
